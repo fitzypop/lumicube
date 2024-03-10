101f18a4f20dd0e6ef6ca41d315156fb063ef6e0
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-03-09 21:46:05 -0700

    further simplify script and shrink timedelta to 1 minute

--- bitcoin_ticker.py
+++ bitcoin_ticker.py
@@ -8,13 +8,17 @@ Color meanings:
 
 from datetime import datetime, timedelta
 import requests
-import math
 import time
-from typing import Any
 
+from money import Money
 
-BUY_PRICE = 25_000
-SELL_PRICE = 60_000
+# Normal Price targets
+# BUY_PRICE = 25_000
+# SELL_PRICE = 60_000
+
+# 3/9/24 - BTC hit $70k and is holding at $68k, need more fine grain targets
+SELL_PRICE = 70_000
+BUY_PRICE = 67_999
 
 BLACK = 0x00000
 BLUE = 0x0000FF
@@ -27,61 +31,36 @@ WHITE = 0xFFFFFF
 display.set_all(BLACK)
 
 
-def parse_to_money(in_num: float) -> tuple[str, Any]:
-    price_int = math.ceil(in_num)
-    in_str = str(price_int)
-    i_len = len(in_str)
-
+def parse_and_get_color(price):
     # Buy Color
-    if price_int <= BUY_PRICE:
+    if price <= BUY_PRICE:
         color = GOLD
 
     # Hodl color
-    elif BUY_PRICE < price_int < SELL_PRICE:
+    elif BUY_PRICE < price < SELL_PRICE:
         color = BLUE
 
     # Sell color
-    elif price_int >= SELL_PRICE:
+    elif price >= SELL_PRICE:
         color = GREEN
 
-    # money string formatting
-    if i_len == 7:
-        price = f"${in_str[:1]}m"
-    if i_len == 6:
-        price = f"${in_str[:3]}k"
-    elif i_len == 5:
-        price = f"${in_str[:2]}k"
-    elif i_len == 4:
-        price = f"${in_str[:1]}k"
     else:
-        price = in_str
-    return price, color
+        color = WHITE
+
+    return Money(price, "USD").format("en_US"), color
 
 
-def get_btc_price() -> tuple[str, Any]:
+def get_btc_price():
     response = requests.get("https://api.coincap.io/v2/assets/bitcoin")
     price = float(response.json()["data"]["priceUsd"])
     print(f"BTC ${price:.2f}  {start}")
-    return parse_to_money(price)
+    return parse_and_get_color(price)
 
-
-# TODO: 8x8 arrays for top panel display
-
-buy = [
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-    [BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK, BLACK],
-]
 
 if __name__ == "__main__":
     start = datetime.now()
     price, color = get_btc_price()
-    wait_time = timedelta(minutes=2)
+    wait_time = timedelta(minutes=1)
 
     while True:
         # display.set_panel("top", buy)
